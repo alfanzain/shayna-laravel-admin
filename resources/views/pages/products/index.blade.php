977790576b1bfa3821591942d35b16e4b8c508b8
Alfan Zain <alfanzainkp@gmail.com>
2020-09-24 06:31:21 +0700

    https://class.buildwithangga.com/course_playing/full-stack-web-laravel-vue-js/71

--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -7,45 +7,48 @@
                 <div class="card">
                     <div class="card-body">
                         <h4 class="box-title">Daftar Barang</h4>
-
-                        <div class="card-body--">
-                            <div class="table-stats order-table ov-h">
-                                <table class="table">
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>Name</th>
-                                            <th>Type</th>
-                                            <th>Price</th>
-                                            <th>Quantity</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>1</td>
-                                            <td>Nama</td>
-                                            <td>Tipe</td>
-                                            <td>0</td>
-                                            <td>0</td>
-                                            <td>
-                                                <a href="#" class="btn btn-info btn-sm">
-                                                    <i class="fa fa-picture-o"></i>
-                                                </a>
-                                                <a href="#" class="btn btn-primary btn-sm">
-                                                    <i class="fa fa-pencil"></i>
-                                                </a>
-                                                <form action="#" method="POST" class="d-inline">
-                                                    @method('delete')
-                                                    <button class="btn btn-danger btn-sm">
-                                                        <i class="fa fa-trash"></i>
-                                                    </button>
-                                                </form>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
+                    </div>
+                    <div class="card-body--">
+                        <div class="table-stats order-table ov-h">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Name</th>
+                                        <th>Type</th>
+                                        <th>Price</th>
+                                        <th>Quantity</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @forelse ($products as $product)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $product->name }}</td>
+                                        <td>{{ $product->type }}</td>
+                                        <td>{{ $product->price }}</td>
+                                        <td>{{ $product->quantity }}</td>
+                                        <td>
+                                            <a href="#" class="btn btn-info btn-sm">
+                                                <i class="fa fa-picture-o"></i>
+                                            </a>
+                                            <a href="#" class="btn btn-primary btn-sm">
+                                                <i class="fa fa-pencil"></i>
+                                            </a>
+                                            <form action="#" method="POST" class="d-inline">
+                                                @method('delete')
+                                                <button class="btn btn-danger btn-sm">
+                                                    <i class="fa fa-trash"></i>
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                    @empty
+                                        
+                                    @endforelse
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
